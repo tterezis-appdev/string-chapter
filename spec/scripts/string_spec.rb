d760e24652d4cb376e96cd45d98b79f77657db0a
Ben Purinton <ben.purinton@gmail.com>
2023-02-03 22:35:04 +0000

    string_addition with tests

--- spec/scripts/string_spec.rb
+++ spec/scripts/string_spec.rb
@@ -1,13 +1,18 @@
+# string_concat
 describe "string_concat.rb" do
   it "should output 'Ruby's fun' using concat", points: 1 do
     concat_file = "string_concat.rb"
     file_contents = File.read(concat_file)
-    concat_counter = 0
+    
     File.foreach(concat_file).with_index do |line, line_num|
       if line.match(/^p.*"Ruby's fun"/) || line.include?("puts")
         expect(line).to_not match(/Ruby's fun/),
           "Expected 'string_concat.rb' to NOT just pring the String literal 'Ruby's fun', but did."
       end
+    end
+
+    concat_counter = 0
+    File.foreach(concat_file).with_index do |line, line_num|
       if line.include?(".concat(") 
         unless line.include?("#")
           concat_counter += 1
@@ -16,39 +21,88 @@ describe "string_concat.rb" do
     end
     expect(concat_counter).to be > 0,
       "Expected 'string_concat.rb' to use the .concat method, but it did not."
+
     output = with_captured_stdout { require_relative('../../string_concat')} 
     output = "empty" if output.empty? 
     expect(output.match?(/Ruby's fun/)).to be(true), "Expected output to be 'Ruby's fun', but was #{output}"
   end
 end
 
+
+# string_addition
+describe "string_addition.rb" do
+  it "should output 'Add it together' using String addition", points: 1 do
+    addition_file = "string_addition.rb"
+    file_contents = File.read(addition_file)
+    
+    File.foreach(addition_file).with_index do |line, line_num|
+      if line.match(/^p.*"Add it together"/) || line.include?("puts")
+        expect(line).to_not match(/Add it together/),
+          "Expected 'string_addition.rb' to NOT print the String literal 'Add it together', but did."
+      end
+    end
+    
+    assignment_counter = 0
+    File.foreach(addition_file).with_index do |line, line_num|
+      if line.include?("=") 
+        unless line.include?("#")
+          assignment_counter += 1
+        end
+      end
+    end
+    expect(assignment_counter).to be >= 2,
+      "Expected 'string_addition.rb' to use at least two variables to store parts of the string before adding, but only #{assignment_counter} '=' found."
+    
+    plus_counter = 0
+    File.foreach(addition_file).with_index do |line, line_num|
+      if line.include?("+") 
+        unless line.include?("#")
+          plus_counter += 1
+        end
+      end
+    end
+    expect(plus_counter).to be >= 1,
+      "Expected 'string_addition.rb' to use the + plus method, but only #{plus_counter} '+' found."
+    
+    output = with_captured_stdout { require_relative('../../string_addition')} 
+    output = "empty" if output.empty? 
+    expect(output.match?(/Add it together/)).to be(true), "Expected output to be 'Add it together', but was #{output}"
+  end
+end
+
+
+# string_multiplication
 describe "string_multiplication.rb" do
   it "should output 'HoHoHo' using String multiplication", points: 1 do
     multiplication_file = "string_multiplication.rb"
     file_contents = File.read(multiplication_file)
+
     File.foreach(multiplication_file).with_index do |line, line_num|
       if line.match(/^p.*"HoHoHo"/) || line.include?("puts")
         expect(line).to_not match(/HoHoHo/),
           "Expected 'string_multiplication.rb' to NOT print the String literal 'HoHoHo', but did."
       end
     end
+
     output = with_captured_stdout { require_relative('../../string_multiplication')} 
     output = "empty" if output.empty? 
     expect(output.match?(/HoHoHo/)).to be(true), "Expected output to be 'HoHoHo', but was #{output}"
   end
 end
 
+
+# string_strip
 describe "string_strip.rb" do
   it "should output 'remove outside spaces'", points: 1 do
-
     output = with_captured_stdout { require_relative('../../string_strip')} 
     output = "empty" if output.empty? 
-
     expect(output.length).to be <= 28, "Expected output to be 'remove the outside spaces', but output was #{output.length} characters long."
     expect(output.match?(/remove the outside spaces/)).to be(true), "Expected output to be 'remove the outside spaces', but was #{output}"
   end
 end
 
+
+# string_case
 describe "string_case.rb" do
   it "should output 'HELLO friends AnD FaMiLy'", points: 1 do
     output = with_captured_stdout { require_relative('../../string_case')} 
@@ -57,6 +111,8 @@ describe "string_case.rb" do
   end
 end
 
+
+# string_gsub
 describe "string_gsub.rb" do
   it "should output 'put spaces in between these words'", points: 1 do
     output = with_captured_stdout { require_relative('../../string_gsub')} 
@@ -65,6 +121,8 @@ describe "string_gsub.rb" do
   end
 end
 
+
+# string_chomp
 describe "string_chomp.rb" do
   it "should remove ending '$'", points: 1 do
     output = with_captured_stdout { require_relative('../../string_chomp')} 
@@ -73,6 +131,8 @@ describe "string_chomp.rb" do
   end
 end
 
+
+# string_gets
 describe "string_gets.rb" do
   it "should output 'Hello, name!'" , points: 1 do
     allow_any_instance_of(Object).to receive(:gets).and_return("jelani\n")
@@ -82,6 +142,7 @@ describe "string_gets.rb" do
     expect(output.match?(/Hello, Jelani!/)).to be(true), "Expected output to be 'Hello, Jelani!', but was #{output}."
   end
 end
+
 
 def with_captured_stdout
   original_stdout = $stdout  # capture previous value of $stdout
--- string_addition.rb
+++ string_addition.rb
@@ -3,7 +3,16 @@
 # "Add it together"
 # 
 # using at least one String addition character.
+# Use at least two variables to store the parts 
+# of the string before you add them together.
 # 
 # (Don't just print the String literal `"Add it together"`)
 
-p "Add"
+first_part = "Add"
+
+
+#### SOLUTION:
+
+# first_part = "Add"
+# second_part = " it together"
+# puts first_part + second_part
