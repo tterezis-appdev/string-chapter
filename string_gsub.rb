e3605c7440b605da2ba7d102b999bbb158b9aa17
Ben Purinton <ben.purinton@gmail.com>
2023-02-04 19:49:24 +0000

    remove print from strip

--- string_gsub.rb
+++ string_gsub.rb
@@ -4,6 +4,4 @@
 # 
 # using the given starting variable.
 
-sentence = "put_spaces_in_between_these_words".gsub(/[^a-z]/i, " ")
-
-p "put spaces in between these words" 
+sentence = "put_spaces_in_between_these_words"
--- string_strip.rb
+++ string_strip.rb
@@ -5,5 +5,3 @@
 # using the given starting variable.
 
 outside_spaces = "    remove the outside spaces    "
-
-p outside_spaces
